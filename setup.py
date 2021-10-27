158a55877a4c2bb59c7fe04a8dc8df75f9497b77
Stefan H. Holek <stefan@epy.co.at>
2021-10-27 18:40:41 +0200

    Move metadata to setup.cfg.

--- setup.py
+++ setup.py
@@ -1,37 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-version = '1.5'
-
-setup(name='pygments-openssl',
-      version=version,
-      description='Syntax coloring for OpenSSL configuration files',
-      long_description=open('README.rst').read() + '\n' +
-                       open('CHANGES.rst').read(),
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Plugins',
-          'Intended Audience :: Developers',
-          'Intended Audience :: End Users/Desktop',
-          'Intended Audience :: System Administrators',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
-          'Topic :: Text Processing :: Filters',
-      ],
-      keywords='sphinx pygments openssl conf lexer openssl.cnf',
-      author='Stefan H. Holek',
-      author_email='stefan@epy.co.at',
-      url='https://github.com/stefanholek/pygments-openssl',
-      license='BSD-2-Clause',
-      packages=find_packages(),
-      install_requires=[
-          'setuptools',
-          'pygments',
-      ],
-      entry_points={
-          'pygments.lexers': 'openssl=pygments_openssl.lexer:OpenSSLConfLexer',
-      },
-      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
-)
+setup()
