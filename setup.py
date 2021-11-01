1b8c5f8edc1dc710fbdbcc5c3b0222ca3a9f0a84
Stefan H. Holek <stefan@epy.co.at>
2021-11-01 20:03:05 +0100

    Add python_requires.

--- setup.py
+++ setup.py
@@ -34,4 +34,5 @@ setup(name='pygments-openssl',
       entry_points={
           'pygments.lexers': 'openssl=pygments_openssl.lexer:OpenSSLConfLexer',
       },
+      python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
 )
